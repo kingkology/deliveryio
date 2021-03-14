d2bbef0ebe37b18ce898c5f1ea99cfccbd4a2fc1
kingkology <43090279+kingkology@users.noreply.github.com>
2021-03-14 16:38:33 +0000

    icon change

--- views/signup/index.php
+++ views/signup/index.php
@@ -126,7 +126,7 @@
                                 </a>
                             </div>
                             <div class="card-footer">
-                                <a href="javascript:void(0);" type="button" class="mb-2 btn btn-outline-info" data-toggle="modal" data-target="#benefits"><i class="fa fa-eye"></i> </a>
+                                <a href="javascript:void(0);" type="button" class="mb-2 btn btn-outline-info" data-toggle="modal" data-target="#benefits"><i class="fa fa-info"></i> </a>
                             </div>
                             <div class="card-footer">
                                 <a href="partner.php" type="button" class="mb-2 btn btn-outline-danger">Signup as partner </a>
@@ -145,7 +145,7 @@
                                 </a>
                             </div>
                             <div class="card-footer">
-                                <a href="javascript:void(0);" type="button" class="mb-2 btn btn-outline-info" data-toggle="modal" data-target="#benefits"><i class="fa fa-eye"></i> </a>
+                                <a href="javascript:void(0);" type="button" class="mb-2 btn btn-outline-info" data-toggle="modal" data-target="#benefits"><i class="fa fa-info"></i> </a>
                             </div>
                             <div class="card-footer">
                                 <a href="driver.php" type="button" class="mb-2 btn btn-outline-danger">Signup as driver </a>
@@ -167,7 +167,7 @@
                                 </a>
                             </div>
                             <div class="card-footer">
-                                <a href="javascript:void(0);" type="button" class="mb-2 btn btn-outline-info" data-toggle="modal" data-target="#benefits"><i class="fa fa-eye"></i> </a>
+                                <a href="javascript:void(0);" type="button" class="mb-2 btn btn-outline-info" data-toggle="modal" data-target="#benefits"><i class="fa fa-info"></i> </a>
                             </div>
                             <div class="card-footer">
                                 <a href="client.php" type="button" class="mb-2 btn btn-outline-danger">Signup as client </a>
